c9914fae44fd996cf2bb30381a1f02552e3caa31
Katie Frances <katiefrances@alex-mac.local>
2024-08-18 20:56:15 +1000

    Added Project Folder and first word game

---
+++ js/words.js
@@ -0,0 +1,43 @@
+let wordList = [
+    {
+        word: "sheikah slate",
+        hint: "In ‘Breath of the Wild’, which device helps Link navigate and access Sheikah Towers?"
+    },
+    {
+        word: "epona",
+        hint: "What is the name of Link’s horse in many games?"
+    },
+    {
+        word: "skyward sword",
+        hint: "Which game introduced the ‘Stamina Wheel’ for Link?"
+    },
+    {
+        word: "riju",
+        hint: "Who is the leader of the Gerudo in ‘Breath of the Wild’?"
+    },
+    {
+        word: "sheikah",
+        hint: "Which ancient civilization’s technology is central to ‘Breath of the Wild’?"
+    },
+    {
+        word: "link's awakening",
+        hint: "What is the title of the 2019 remake of a 1993 Zelda game?"
+    },
+    {
+        word: "tera",
+        hint: "In ‘Breath of the Wild’, who is the Great Fairy of the desert?"
+    },
+    {
+        word: "goron",
+        hint: "Which tribe resides in Death Mountain in various Zelda games?"
+    },
+    {
+        word: "king rhoam",
+        hint: "In ‘Breath of the Wild’, who gives Link the Paraglider?"
+    },
+    {
+        word: "koroks",
+        hint: "In ‘Breath of the Wild’, what creatures provide hints and sell items?"
+    },
+
+]
